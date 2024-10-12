a949e47b425314dc174129f502bce65531f2e0f8
Srishti-050 <ss6140@srmist.edu.in>
2024-10-12 15:43:58 +0530

    Add files via upload

---
+++ natural no from 10-20.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    //declaring variable
+    int i=0;
+    //using while loop
+    while (i<=20)
+    {
+        //using nested if loop
+        if (i>=10)
+        {
+            //printing output
+            printf("The value of i is %d \n",i);
+        }
+        //increment of i value
+        i++;
+    }
+    return 0;
+}
